35000ce45c5d242b3d7134aa13b1fd50104d1ece
Tanmay Daga <tanmay06daga@gmail.com>
2024-05-27 23:47:19 +0530

    visitIITJ Navlinks added

--- controllers/visitIITJ.js
+++ controllers/visitIITJ.js
@@ -1,9 +1,10 @@
 
 
-
+const CONSTANTS = require("../utils/constants");
 module.exports = {
   visitIITJView: async (req, res, next) => {
-    res.render("visitIITJ" );
+    res.render("visitIITJ",{ [CONSTANTS.HOME_PAGE_RENDER_INPUTS.NavLinks]:
+      CONSTANTS.HOME_PAGE_NAVLINKS,} );
     next();
   },
 };
