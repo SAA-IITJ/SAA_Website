9a3338638835df3c0f482f174defd7acb0c1169f
Tanmay Daga <tanmay06daga@gmail.com>
2024-05-27 22:48:58 +0530

    fa added team members

--- controllers/facultyAdvisor.js
+++ controllers/facultyAdvisor.js
@@ -4,6 +4,20 @@ module.exports = {
     res.render("facultyAdvisor", {
       [CONSTANTS.HOME_PAGE_RENDER_INPUTS.NavLinks]:
         CONSTANTS.HOME_PAGE_NAVLINKS,
+      [CONSTANTS.TEAM_PAGE_RENDER_INPUTS.TeamMembers]:
+        await TeamMembers.findAll({
+          attributes: {
+            exclude: ["createdAt", "updatedAt"],
+          },
+        }).then((data) => {
+          return data.map((ele) => {
+            return {
+              ...ele.dataValues,
+              position: getTeamPagePositionType(ele.dataValues.position),
+              teamType: getTeamPageTeamType(ele.dataValues.teamType),
+            };
+          });
+        }),
     });
     next();
   },
