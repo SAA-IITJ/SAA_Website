6ab7df37f26c612b348d2ad084e285e99d6eef9e
Tanmay Daga <tanmay06daga@gmail.com>
2024-05-27 22:08:38 +0530

    create Faculty Advisor route and view

---
+++ controllers/facultyAdvisor.js
@@ -0,0 +1,11 @@
+
+
+module.exports = {
+    facultyAdvisorView: async (req, res, next) => {
+      res.render("facultyAdvisor");
+      next();
+    },
+  };
+  
+  
+  
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const router = require("express").Router();
 router.use("/", require("./home"));
 router.use("/", require("./team"));
 router.use("/", require("./events"));
-router.use("/",require("./visitIITJ"))
+router.use("/", require("./visitIITJ"));
+router.use("/", require("./facultyAdvisor"));
 module.exports = router;
