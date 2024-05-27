952ad857df601c67550ca7e367e2c53a8e6aa29f
Tanmay Daga <tanmay06daga@gmail.com>
2024-05-27 22:44:05 +0530

    added navlinks to fa

--- controllers/facultyAdvisor.js
+++ controllers/facultyAdvisor.js
@@ -1,11 +1,9 @@
-
-
 module.exports = {
-    facultyAdvisorView: async (req, res, next) => {
-      res.render("facultyAdvisor");
-      next();
-    },
-  };
-  
-  
-  
+  facultyAdvisorView: async (req, res, next) => {
+    res.render("facultyAdvisor", {
+      [CONSTANTS.HOME_PAGE_RENDER_INPUTS.NavLinks]:
+        CONSTANTS.HOME_PAGE_NAVLINKS,
+    });
+    next();
+  },
+};
